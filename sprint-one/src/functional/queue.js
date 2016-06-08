0fba1e4da6a5365b78f4021282928c0c0271ae75
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 15:36:26 -0700

    "Allow sequentially adding and removing items"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,24 +1,34 @@
+ //var testObj= {0: 'test', 1: 'test2', 2:'test3'};
+
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-var key=0;
+var counter=0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    key++;
-
+    counter++;
+    for (var key in storage){
+      var number=Number(key)+1;
+      storage[number]=storage[key];
+    }
+storage[0]=value;
+return storage;
   };
 
   someInstance.dequeue = function() {
-    if (key>0){
-    key--;
+    if (counter>0){
+    counter--;
+    return storage[counter];
      }
+
   };
 
   someInstance.size = function() {
-    return key;
+    return counter;
   };
 
   return someInstance;
