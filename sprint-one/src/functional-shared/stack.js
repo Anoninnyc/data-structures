45977d57ee8daff832d7ce14c95e5ac2a2336fe4
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 16:13:44 -0700

    "Reports a size of zero for a new stack"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
+
+
+
+
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var counter=0;
+  var newStack={};
+  _.extend(newStack, stackMethods);
+
+  return newStack;
+
 };
 
-var stackMethods = {};
+
+var stackMethods = {
 
 
+};
+
+
+stackMethods.size= function(){
+ return 0;
+ }
+
+ 
