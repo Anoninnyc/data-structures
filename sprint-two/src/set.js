d782face5f938f99a37b9ce67c183860818b4762
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 16:09:04 -0700

    "Complete set"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item]=item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item]){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+
+  for (var key in this._storage){
+    if (this._storage[key]===item){
+      this._storage[key]=null;
+
+    }
+  }
+
+
+
+
+
 };
 
 /*
