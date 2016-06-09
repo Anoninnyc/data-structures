2045706a72ff7e0150261b2239038a8df6a9aa33
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 19:36:21 -0700

    "Finish functional-shared pattern"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,3 +1,5 @@
+
+
 var Queue = function() {
  var newQueue= {};
  newQueue.counter=0;
@@ -15,4 +17,22 @@ queueMethods.size=function(){
   return this.counter;
 }
 
+queueMethods.enqueue=function(value){
 
+  for (var key in this.storage){
+    this.storage[Number(key) + 1]=this.storage[key];
+  }
+
+  this.storage[0]=value;
+  this.counter++;
+  }
+
+
+queueMethods.dequeue=function(){
+
+if (this.counter>0){
+  this.counter--;
+  return this.storage[this.counter];
+}
+
+};
