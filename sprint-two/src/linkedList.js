3807db3f842a50ddff3fcb09750a75da81c332d8
Hack Reactor Students <accounts@hackreactor.com>
2016-06-09 10:59:27 -0700

    "Complete LinkedList"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,48 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail) {
+      list.tail.next  = Node(value);
+    }
+    list.tail= Node(value);
+    if (list.head === null){
+      list.head = list.tail;
+    }
+    
+
   };
 
   list.removeHead = function() {
+    var prevHead = list.head;
+    list.head = list.head.next;
+    return prevHead.value;
+
   };
 
-  list.contains = function(target) {
-  };
+  list.contains = function(target,node) {
+    var node = node || list.head;
+    while (node.value !== list.tail.value){
+        if (node.value === target){
+          return true;
+        } 
+        else if (list.tail.value === target){
+          return true;
+        } else {
+          return list.contains(target, node.next);
+        }
+      };
+      return false;
+    };
 
   return list;
 };
+
+
+
+
+
+
+
 
 var Node = function(value) {
   var node = {};
@@ -27,3 +59,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
