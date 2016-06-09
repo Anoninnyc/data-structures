0180d11c1703873b4672ec360abcddc57eefcbc7
Hack Reactor Students <accounts@hackreactor.com>
2016-06-08 20:08:30 -0700

    "Complete Data Structure Sprint 1"

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage={};
+  this.counter=0;
 };
+
+
+Queue.prototype.enqueue= function(value){
+  
+  for (var key in this.storage){
+    this.storage[1+ Number(key)]=this.storage[key];
+
+  }
+  this.storage[0]=value;
+  this.counter++;
+}
+
+Queue.prototype.dequeue= function(){
+  if (this.counter>0){
+  this.counter--;
+  return  this.storage[this.counter];
+}
+}
+
+
+Queue.prototype.size= function(){
+  return this.counter;
+}
+
 
 
