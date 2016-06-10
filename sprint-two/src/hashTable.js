82ad87782ce3e4cac5a23c7a55a422b939c33611
Hack Reactor Students <accounts@hackreactor.com>
2016-06-10 15:55:37 -0700

    "Redo HTs / Finish Basic Requirements"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,80 +5,64 @@ var HashTable = function() {
 
 
 HashTable.prototype.insert = function(k, v) {
-var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
 
 
-if (this._storage[index]===undefined){
-this._storage[index] = [];
-this._storage[index].push([k,v]);
-};
 
 
-for (var i=0;i<8;i++){
-  if (this._storage[i]){
-  for (var x=0;x<this._storage[i].length;x++){  
-    if (this._storage[i].length>0 &&this._storage[i][x][0]===k){
-     this._storage[i][x]= [k,v];
-    }
 
-  if (this._storage[i]&&this._storage[i][x][0]!==k){
-  this._storage[i].push([k,v]);
-  };
-
-}
+  if (this._storage[index] === undefined) {
+    this._storage[index] = [
+      [k, v]
+    ];
   }
+
+
+
+
+
+
+  if (this._storage[index]) {
+
+    for (var i = 0; i < this._storage[index].length; i++) {
+
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i] = [k, v];
+      }
+      if (this._storage[index][i][0] !== k) {
+        this._storage[index].push([k, v]);
+      }
+    }
+  }
+
 }
 
 
+//[[[1,2]],[[3,4]],[[5,6],['a',b]],[[7,8]],[k,v]]]
 
-
-
-};
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //[[5,6],['a',b]]
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
 
- for (var i=0; i<8; i++){
-  if (this._storage[i]){
-for (var x=0;x<this._storage[i].length;x++){
-  if (this._storage[i][x][0] === k){
-    return this._storage[i][x][1];
   }
-}
-}
-}
+
+
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-
-
- for (var i=0; i<8; i++){
-  if (this._storage[i]){
-  for (var x=0;x<this._storage[i].length;x++){
-  if (this._storage[i][x][0] === k){
-    this._storage[i][x]=[undefined,undefined];
-  }
-}
-}
-}
-
-
-
-
-
-
+  this._storage[index][0][1] = undefined;
 
 
 
 
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
 
 
